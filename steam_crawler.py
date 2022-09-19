591001d5c29cc8cc54aec308ccf4e6cdbd5a1b7c
HillOfMay <35472882+HillOfMay@users.noreply.github.com>
2022-09-19 18:16:31 +0900

    스팀크롤링 코드 준완성본!!

---
+++ steam_crawler.py
@@ -0,0 +1,126 @@
+import pandas as pd
+import requests
+import json
+
+def get_steam_rev(app_list:list, stop_time:int = 0)->tuple[pd.DataFrame, pd.DataFrame]:
+    '''
+    스팀 API 활용 리뷰 수집 코드!!
+
+    사용한 방법: steamworks api, 하루 최대 1000만 건, 시간당 약 120만건 수집 가능(0.5 query/s)
+
+    Input:
+        app_list: 수집할 게임의 id 리스트입니다.
+        stop_time: 리뷰의 수집 일자를 제한하는 Timestamp 형식의 시간입니다.
+
+    Output:
+        sumaries_df: 수집한 게임별 리뷰 통계입니다.
+
+          ========================================================================
+          num_reviews: 리뷰의 쿼리 당 수집량
+          review_score: 수집한 리뷰들의 긍정평가 비율
+          review_score_desc: 스팀에서 표시되는 평가 텍스트(예시: 대체로 긍정적)
+          total_positive: 긍정적 평가의 수
+          total_negative: 부정적 평가의 수
+          total_reviews: 수집된 리뷰의 총량
+          =========================================================================
+
+
+
+        rev_df_all: 수집한 게임들의 리뷰들 Dataframe입니다.
+
+          =========================================================================
+          recommendationid: 리뷰의 고유 id
+          author: 리뷰 작성자에 대한 정보 (플레이타임 등)
+          language: 작성된 리뷰의 언어
+          review: 리뷰 본문
+          timestamp_created: 리뷰 작성 시각(unix Timestamp)
+          timestamp_updated: 리뷰 최종 수정 시각
+          voted_up: True일 시, 긍정 평가
+          votes_up: 유저들에게서 받은 추천수
+          votes_funny: 유저들에게서 받은 "재미있음" 수
+          weighted_vote_score: 스팀에서 평가한 리뷰의 "유용함" 지표
+          comment_count: 달린 댓글의 수
+          steam_purchase: True일 시, 작성자의 게임은 스팀 직접 구매입니다.(False는 선물 혹은 CD키 구매입니다)
+          received_for_free: True일 시, 작성자의 게임은 'Steam 선물'을 통해 받은 게임입니다.
+          written_during_early_access: True일 시, 게임의 '앞서 해보기' 기간동안 작성된 리뷰입니다. '앞서 해보기'란 일종의 유료 베타를 의미합니다.
+          hidden_in_steam_china: True일 시, 중국에서 금지된 게임입니다.
+          =========================================================================
+    '''
+
+
+
+    rev_df_all = pd.DataFrame()
+
+    for app in app_list:    #crwal all game in list
+        url = f"https://store.steampowered.com/appreviews/{app}?json=1"
+        params = {"filter":"recent",
+                "language":"english",
+                "cursor":"*",
+                "review_type":"all",
+                "purchase_type":"all",
+                "num_per_page":"100"
+              }
+        revs = []
+        summaries = []
+        num_reviews = 200
+
+        while num_reviews!=0:
+        
+          for tr in range(5): #if error, retry
+            res = requests.get(url, params=params).text
+            res = json.loads(res) 
+
+            if not res['success']:err_input = input() #failue
+            else:break
+
+          if not res['query_summary']['num_reviews'] == 100: print(app+" passed??")
+
+          if res['reviews'][-1]['timestamp_created'] <stop_time : break #outdated
+
+          if params['cursor']=='*': 
+                sum = {"app_id":app}
+                sum.update(res['query_summary']) #get summary
+                summaries += [sum]
+
+          revs += res['reviews'] #get reviews
+
+
+          num_reviews = res['query_summary']['num_reviews'] #finished check
+          params['cursor'] = res['cursor']
+
+          #test code 
+          if len(revs)>300:break
+
+        rev_df = pd.DataFrame(revs)
+        rev_df = rev_df[rev_df.timestamp_created>stop_time] #kill outdated
+        rev_df.insert(0, "app_id", app) #insert game id
+
+        rev_df_all = pd.concat([rev_df_all, rev_df]) 
+    summaries_df = pd.DataFrame(summaries)
+    return summaries_df, rev_df_all 
+
+
+
+#크롤링 사이트 선정 기준
+
+'''
+1, 전문성 - 수집한 리뷰에 관련없는 데이터(트롤러들)가 얼마나 덜 포함되었을지 판단하는 지표
+
+(10/10), 게임 정보만 허용되는가? 3/회원제인가? 2/ 게임 구매자만 작성하냐? 3/전문 기자가 작성하냐? 2/
+
+s8 r5 c3 m6 v10 
+
+2, 정보량(?) - 수집한 리뷰가 식별 정보를 얼마나 충실하게 포함하고 있는지 나타내는 지표(추가로 포함된 메타정보에 가산점)
+
+(10/10), 날짜, 작성자, 좋아요, 평점, 
+3, 접근성 - 수집할 리뷰를 얼마나 수월하게 수집할 수 있는지 나타내는 지표
+4, 리뷰 수 - 수집해 올 수 있는 리뷰의 갯수
+5, 활성도 - 사이트가 게임의 변화을 얼마나 반영할 수 있는지 나타내는 지표
+
+
+사이트별 한줄평?
+
+1, Steam
+    (분석 대상의)게임을 구매하기 위해 반드시 이용해야 하는 플랫폼이며, 
+
+'''
