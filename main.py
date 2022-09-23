ecf37e69c7afc80ab4ebe097678b4d9cd63aaaa8
HillOfMay <35472882+HillOfMay@users.noreply.github.com>
2022-09-23 15:56:23 +0900

    임시 main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+from steam_crawler import get_steam_rev
+import meta_crawler
+
+def main():
+    game_df = pd.read_csv("games_list.csv")
+    
+    test_summary, test_df = get_steam_rev(game_df["appid"][0:100], 0)
+    
+    test_summary.to_csv("1_100_summary.csv")
+    test_df.to_csv("1_100_review.csv")
+
+if __name__=="__main__":
+    main()
